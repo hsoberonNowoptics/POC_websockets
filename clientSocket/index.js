020887295fb3d6d5b6d5f724ec7d52c15b84a5da
Hernan Soberon <hernan.d.soberon@nowoptics.com>
2024-08-08 16:05:50 -0500

    Ping working, backup working TEST 1 COMPLETED

--- clientSocket/index.js
+++ clientSocket/index.js
@@ -1,112 +1,144 @@
 /**
- * LANE 1 WS CLIENT 
+ * LANE AS WS CLIENT 
 */
 import { WebSocketServer, WebSocket } from "ws";
+import crypto from 'crypto'
 
+    //Client ID
+    const cid = crypto.randomUUID().split('-')[0];
+    console.log('Client ID: ' + cid);   
     
-
     //line one info with any data. 
     const lineData = {
-        id : 8091,
-        title : 'Line 1',
-        message : 'Hello from Line 1',
+        cid : cid,
+        message : 'Hello from Line',
         description : 'The #1 line in the fleet.'
     }
 
 
     const serverPorts = [8091, 8092, 8093];
     var sPort = 0;
-
-    var bulbsList = [];
-
     var wss; 
+
+    let web_clients = [];
 
     newConnection(sPort);
 
-/* ********** SERVER **********  */
 
-    
+    /* ********** CLIENT **********  */
+
+
+    //Connnection to WEBSOCKET CENTRAL
+    var ws = new WebSocket('ws://localhost:8080/?cid='+cid);
+
+    ws.on('open', function() {
+        //send the socket info
+        // ws.send(JSON.stringify(lineData));
+        console.log('Connected to Grand Central');
+    });
+
+    ws.on("message", function message(data) {
+        
+        //the message is on JSON format 
+        const info = JSON.parse(data)
+        console.log(info.message);
+
+        //Validation of optional data
+        if(info.description) {
+            console.log(info.description);
+        }
+
+        if(info.to){
+            const sendTo = web_clients.filter(s => s.id == info.to )
+            
+            if(sendTo.length > 0){
+                sendTo[0].ws.send(JSON.stringify(info));
+            }
+        }
+        
+    });
+
+
+    ws.on("close", function(event) {
+        // console.log(JSON.stringify(event));
+        
+        var ws2 = new WebSocket('ws://localhost:8081');
+
+        ws2.on("open", function(){
+            console.log("Reconnect to backup server");
+        
+            // console.log("Sending a message to 8092");
+            // lineData.to = 8092;
+
+            // ws2.send(JSON.stringify(lineData));
+        })
+        
+    });
+
+
+
+    /* ********** SERVER **********  */
+
 
     function newConnection(sPort){
+        console.log("Starting on port: " + serverPorts[sPort]);
         wss = new WebSocketServer({ port: serverPorts[sPort] });
-
+        
         wss.on('error', function(){
+            console.log('Port already in use');
             sPort++;
             if(sPort < serverPorts.length){
                 newConnection(sPort);
             }
         })
 
-        wss.on("connection", function connection(wsweb) {
-
-            console.log('New Connection from webpage');
-    
-            //Send the central socket info to the new stablished connection. 
+        wss.on("connection", function connection(wsweb, req) {
+            //Send the new conenction socket the info of this client. 
             lineData.status = 1;
+            lineData.id = serverPorts[sPort];
+            lineData.message = 'Hello from Line ' + serverPorts[sPort],
             wsweb.send(JSON.stringify(lineData));
-        });
 
-        wss.on('message', function message(data) {
-            bulbsList[data.id] = wss;
+            //get info from connection
+            const newUrl = new URL('https://localhost'+req.url);
+            const web_id = newUrl.searchParams.get('id')
+            const web_name = newUrl.searchParams.get('name')
+            console.log('New Connection from : ' + web_name + ' (' + web_id + ')');
 
-            let newMessage = {
-                title: 'Line 1 Connected',
-                message : 'Connection stablished',
-                status: 1
+            //register the new connection
+            const newConn = {
+                id: web_id,
+                name: web_name, 
+                ws: wsweb
             }
-            bulbsList[data.id].send(JSON.stringify(newMessage))
+            web_clients.push(newConn);
+
+            //send the web client to central. 
+            var info = {
+                message: 'New Connection from web page ' + web_id,
+                client_id: cid, 
+                web_id: web_id,
+                webname: web_name 
+            }
+            ws.send(JSON.stringify(info));
+            
+            //if client disconnect, delete from list. 
+            wsweb.on('close', function() {
+                console.log(web_name + ' Closed the connection ' + web_id);
+                web_clients = web_clients.filter(s => s.ws !== wsweb);
+            })
+
+            wsweb.on('message', function(data) {
+                let info = JSON.parse(data)
+                console.log('Message from ' + web_id + ' to CENTRAL');
+                
+                ws.send(data)
+            });
         });
 
-        console.log("Trying to start on port: " + serverPorts[sPort]);
+        
     }
 
-/* ********** CLIENT **********  */
-
-
-//Connnection to WEBSOCKET CENTRAL
-var ws = new WebSocket('ws://localhost:8080');
-
-ws.on('open', function() {
-    //send the socket info
-    // ws.send(JSON.stringify(lineData));
-    console.log('Connected to Grand Central');
-});
-
-ws.on("message", function message(data) {
-    
-    //the message is on JSON format 
-    const info = JSON.parse(data)
-    console.log(info.message);
-
-    //Validation of optional data
-    if(info.description) {
-        console.log(info.description);
-    }
-
-    // //when reciving from the second client, send a response. 
-    // if(info.id == 8092){
-    //     lineData.to = info.id;
-    //     ws.send(JSON.stringify(lineData));
-    // }
-    
-});
-
-
-ws.on("close", function(event) {
-    // console.log(JSON.stringify(event));
-    
-    var ws2 = new WebSocket('ws://localhost:8081');
-
-    ws2.on("open", function(){
-        console.log("Reconnect to backup server");
-    
-        // console.log("Sending a message to 8092");
-        // lineData.to = 8092;
-
-        // ws2.send(JSON.stringify(lineData));
-    })
-    
-});
 
 
 
--- socketCentral/index.js
+++ socketCentral/index.js
@@ -8,18 +8,20 @@ const wss = new WebSocketServer({ port: 8080 });
 console.log("GRAN-CENTRAL WS IS UP")
 
 //List of other WS connected 
-const fleet = [];
+var fleet = [];
 
-wss.on("connection", function connection(ws) {
-    
-    console.log('New Connection');
-
+wss.on("connection", function connection(ws, req) {
     
     const response = {
         id: 8080,
-        title: "Grand Central",
         message : "Welcome to the GranCentral Dispatch"
     }
+
+    //get info from connection
+    const newUrl = new URL('https://localhost'+req.url);
+    const cid = newUrl.searchParams.get('cid')
+
+    console.log('New client Connection id ' + cid);
     
     //Send the central socket info to the new stablished connection. 
     ws.send(JSON.stringify(response));
@@ -28,21 +30,45 @@ wss.on("connection", function connection(ws) {
     //Messages from ws clients
     ws.on('message', function message(data) {
         const info = JSON.parse(data)
-        console.log("Message from: %s \n %s", info.title, info.description);
+        console.log("Message from %s: %s", info.client_id, info.message);
 
-        //when the message contain and id, save this connection on the FLEET array
-        if(info.id){
-            fleet[info.id] = ws;
+        //when the message contain and wclient and webid, save this connection on the FLEET array
+        if(info.client_id && info.web_id){
+            fleet.push({client: info.client_id, web: info.web_id, name:info.webname, ws: ws})
+
+            const sendData = {
+                data: fleet,
+                count: fleet.length,
+                message: 'Connected Web CLientes',
+                type: 'all'
+            }
+            
+            fleet.forEach(item => {
+                sendData.to = item.web;
+                item.ws.send(JSON.stringify(sendData))
+            })
         }
 
         //When the messages contains a TO option, despatch the data to that ws. 
         if(info.to){
             console.log("Sending data to: " + info.to);
 
-            fleet[info.to].send(JSON.stringify(info))
+            const destination = fleet.filter(s => s.web == info.to);
+            if(destination){
+                destination.forEach(item => {
+                    item.ws.send(data);
+                });
+            }
+
+            
             
         }
       });
+
+    ws.on('close', function(){
+        console.log('Clossing client');
+        fleet = fleet.filter(s => (JSON.stringify(s.ws) != JSON.stringify(ws) ) )
+    })
     
     
 });
